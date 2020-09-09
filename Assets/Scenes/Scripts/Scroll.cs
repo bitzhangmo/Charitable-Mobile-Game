e6752f98e4c37eef12e180d1eef57f2b6ecbec95
mosswang <wangxc1998@qq.com>
2020-09-10 00:17:45 +0800

    >> 摇杆，弹球，字体文件

---
+++ Assets/Scenes/Scripts/Scroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scroll : ScrollRect
+{
+    protected float mRadius = 0f;
+
+    protected override void Start()
+    {
+        base.Start();
+        mRadius = (transform as RectTransform).sizeDelta.x * 0.5f;
+    }
+
+    public override void OnDrag(UnityEngine.EventSystems.PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+        var contentPostion = this.content.anchoredPosition;
+        if (contentPostion.magnitude > mRadius)
+        {
+            contentPostion = contentPostion.normalized * mRadius;
+            SetContentAnchoredPosition(contentPostion);
+        }
+    }
+}
