639c5776f1e99838ac9629e2f2beeeefabe3f630
jackeliu <786338523@qq.com>
2020-09-20 14:49:01 +0800

    add login register ui

---
+++ Assets/login/Scripts/btnLogin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class btnLogin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void login () {
+	    print("ChinarOnClickTest");
+        SceneManager.LoadScene(1);//跳转到指定的Level，也就是第一步中的右侧标号
+
+	}
+}
